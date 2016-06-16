d8f65d8576f3d9bc8c1a7ecb179ce235be30bca5
Lei Guangwen <leiguangwen@LeideMacBook-Pro.local>
2016-06-16 01:06:27 -0700

    Initial Commit

---
+++ customer.py
@@ -0,0 +1,10 @@
+class customer:
+    def __init__(self, name, phoneNumber, address):
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.address = address
+    def __str__(self):
+        string = "Name: " + self.name + "\n"
+        string += "Phone Number: " + self.phoneNumber + "\n"
+        string += "Address: " + self.address
+        return string
